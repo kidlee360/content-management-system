96c935a02fc3459b1dc7b201c762157d236126d0
kidlee360 <ugoleevi@gmail.com>
2026-01-16 14:47:50 +0100

    Complete without authentication

--- lib/db.ts
+++ lib/db.ts
@@ -2,14 +2,31 @@ import { Pool } from 'pg';
 import 'dotenv/config';
 
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-});
+// LOCAL POSTGRES CONNECTION
+//const pool = new Pool({
+  //user: process.env.DB_USER,
+  //host: process.env.DB_HOST,
+  //database: process.env.DB_DATABASE,
+  //password: process.env.DB_PASSWORD,
+  //port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+//});
 
+// A quick test function to ensure the connection works
+//pool.connect((err: any, client: any, release: any) => {
+  //if (err) {
+    //return console.error('Error acquiring client', err.stack);
+  //}
+  //console.log('Successfully connected to Postgres database!');
+  //release();
+//});
+
+// LIVE SERVER POSTGRES CONNECTION
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 // A quick test function to ensure the connection works
 pool.connect((err: any, client: any, release: any) => {
   if (err) {
